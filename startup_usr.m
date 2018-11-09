e0b10dd3b97ee8c3ea762ef067fccd8b9844f43d
E00026 <E00026@CTRLGROUP27.win.iaec.isuzu.co.jp>
2018-11-09 14:25:30 +0900

    コンフィギュレーション参照のデータロードを追加

--- startup_usr.m
+++ startup_usr.m
@@ -1,9 +1,17 @@
+% Simulinkプロジェクトを使わずにモデルを開く
+
 %% パスの設定
 addpath(genpath(pwd));
 
 %% 作業フォルダへの移行
-%cd('Working');
-cd('D:\Users\Nishito\MatWork\JmaabVccWs\Jmaab_Veh_WS_HEVmodel\Working');
+cd('Working');
+%cd('D:\Users\Nishito\MatWork\JmaabVccWs\Jmaab_Veh_WS_HEVmodel\Working');
+
+%% load configuration parameter
+Config_Ctrl_MILS
+Config_Ctrl_Module_MILS
+Config_Plant_MILS_FixStep
+%Config_Plant_MILS_VarStep
 
 %% モデルを開く
 HEV_Simu_V1_1;
