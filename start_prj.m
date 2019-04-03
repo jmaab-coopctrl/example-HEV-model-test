b3db4347dff69b56fef1c21513d14ff1d9539d60
E00026 <E00026@CTRLGROUP27.win.iaec.isuzu.co.jp>
2019-04-03 13:12:12 +0900

    start_prj、startup_usrスクリプト更新

--- start_prj.m
+++ start_prj.m
@@ -12,10 +12,16 @@
 %% 作業フォルダへの移行
 cd('Working');
 
-%% load configuration parameter
-Config_Ctrl_MILS
-Config_Ctrl_Module_MILS
-Config_Plant_MILS_FixStep
+%% load configuration parameter from mat file
+load ConfigSet_Ctrl_MILS
+load ConfigSet_Ctrl_Module_MILS
+load ConfigSet_Plant_MILS_FixStep
+%load ConfigSet_Plant_MILS_VarStep
+
+%% load configuration parameter from m file
+%Config_Ctrl_MILS
+%Config_Ctrl_Module_MILS
+%Config_Plant_MILS_FixStep
 %Config_Plant_MILS_VarStep
 
 %% モデルを開く
--- startup_usr.m
+++ startup_usr.m
@@ -1,16 +1,20 @@
-% Simulinkプロジェクトを使わずにモデルを開く
-
 %% パスの設定
 addpath(genpath(pwd));
 
 %% 作業フォルダへの移行
-cd('Working');
-%cd('D:\Users\Nishito\MatWork\JmaabVccWs\Jmaab_Veh_WS_HEVmodel\Working');
+%cd('Working');
+cd('D:\Users\Nishito\MatWork\JmaabVccWs\Jmaab_Veh_WS_HEVmodel\Working');
 
-%% load configuration parameter
-Config_Ctrl_MILS
-Config_Ctrl_Module_MILS
-Config_Plant_MILS_FixStep
+%% load configuration parameter from mat file
+load ConfigSet_Ctrl_MILS
+load ConfigSet_Ctrl_Module_MILS
+load ConfigSet_Plant_MILS_FixStep
+%load ConfigSet_Plant_MILS_VarStep
+
+%% load configuration parameter from m file
+%Config_Ctrl_MILS
+%Config_Ctrl_Module_MILS
+%Config_Plant_MILS_FixStep
 %Config_Plant_MILS_VarStep
 
 %% モデルを開く
