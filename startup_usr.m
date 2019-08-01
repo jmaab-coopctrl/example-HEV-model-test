f8f0c5cb3572648a83c02ee67293b4a9a7409a25
nishito <masaaki-nishito@isuzu.co.jp>
2019-08-01 16:29:17 +0900

    startup_usr.m修正

--- startup_usr.m
+++ startup_usr.m
@@ -2,8 +2,8 @@
 addpath(genpath(pwd));
 
 %% 作業フォルダへの移行
-%cd('Working');
-cd('D:\Users\Nishito\MatWork\JmaabVccWs\Jmaab_Veh_WS_HEVmodel\Working');
+cd('Working');
+%cd('D:\Users\Nishito\MatWork\JmaabVccWs\Jmaab_Veh_WS_HEVmodel\Working');
 
 %% load configuration parameter from mat file
 load ConfigSet_Ctrl_MILS
