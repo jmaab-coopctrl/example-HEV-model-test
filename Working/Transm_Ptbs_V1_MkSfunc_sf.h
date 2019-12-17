82289405bb6170af259d45f3138b82672c7ea085
nishito-isuzu <53589083+nishito-isuzu@users.noreply.github.com>
2019-12-17 16:17:53 +0900

    <Add> Transm_Ptbs_V1_MkSfunc_sf.c, .h, .mexw64  トランスミッションのSファンクションモデルの実行に必要なファイルを追加

---
+++ Working/Transm_Ptbs_V1_MkSfunc_sf.h
@@ -0,0 +1,15 @@
+/*
+* Generated S-function Target for model Transm_Ptbs_V1_MkSfunc. 
+* This file provides access to the generated S-function target
+* export file for other models.
+*
+* Created: Fri Dec 13 08:59:06 2019
+*/
+
+#ifndef RTWSFCN_Transm_Ptbs_V1_MkSfunc_sf_H
+#define RTWSFCN_Transm_Ptbs_V1_MkSfunc_sf_H
+
+#include "Transm_Ptbs_V1_MkSfunc_sfcn_rtw\Transm_Ptbs_V1_MkSfunc_sf.h"
+  #include "Transm_Ptbs_V1_MkSfunc_sfcn_rtw\Transm_Ptbs_V1_MkSfunc_sf_private.h"
+
+#endif
